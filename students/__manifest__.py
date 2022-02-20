c3faa26d8ebb789a795047ace473a8f931f82a54
Nhlakanipho Nkokhelo <N.N.TEMBE@outlook.com>
2022-02-20 19:31:12 +0200

    first module creation

---
+++ students/__manifest__.py
@@ -0,0 +1,24 @@
+{
+  'name': 'students module',
+  'version': '1.0',
+  'description': 'student module for learning odoo',
+  'summary': 'student module for training',
+  'author': 'nkokhelo Tembe',
+  'website': 'www.odoo.com',
+  'license': 'LGPL-3',
+  'category': 'training',
+  'depends': [
+    'base'
+  ],
+  'data': [
+    ''
+  ],
+  'demo': [
+    ''
+  ],
+  'auto_install': False,
+  'application': False,
+  'assets': {
+    
+  }
+}
