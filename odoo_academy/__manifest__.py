42b4f2b3bfc2b4da148c4d6e91d4f21a4191eb02
Nkokhelo <n.n.tembe@outlook.com>
2022-02-20 21:23:19 +0000

    rename module to academy

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,7 +1,7 @@
 {
-  'name': 'students module',
+  'name': 'academy module',
   'version': '1.0',
-  'description': 'student module for learning odoo',
+  'description': 'academy module for learning odoo',
   'summary': 'student module for training',
   'author': 'nkokhelo Tembe',
   'website': 'https://www.odoo.com',
---
+++ odoo_academy/models/course.py
@@ -0,0 +1,6 @@
+from odoo import models, fileds, api;
+
+class Course(models.Model):
+  _name = 'academy.courses'
+  _description = 'Course Info'
+  
